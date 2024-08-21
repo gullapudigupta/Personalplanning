b2788c2bd9f8921fb04f53f703abb8805afccc13
gullapudigupta <satyanarayanaguptag@gmail.com>
2024-08-21 21:42:33 +0530

    Add files via upload

--- solution2/core/hack_reco.py
+++ solution2/core/hack_reco.py
@@ -12,10 +12,15 @@ def createPromt(df_user, df_dest):
     prompt_initial_text = "You are a Personalized travel agent bot who can answer questions about user\'s upcoming travel that is already planned or you will help plan. You will take into account the  user\'s personal data like home city, age, hobbies, interests and favorite food while answering questions. Date format is YYYY-MM-DD. If you do not know the answer you response should be 'Sorry I'm Unsure of that. Is there something else I can help you with?'. \\n"
 
     user_intro_text = str(df_user["user_id"][0]) + " who is interseted in "+df_user["preferred_activities"][0]+".\\n"
-
+    
+    user_destinatins = "\nSuggest the best cruise for user " + str(df_user["user_id"][0]) + " from below options based on user's request:\n"
+    
+    for row_index, row in df_dest.iterrows():
+        user_destinatins += row["operator"] + " cruise to " + (row["name"]) + "(best season to visit: "+(row["best_season_to_visit"])+" , theme:"+row["category"]+")\n"
+    
     addtl_instructions = "Can you answer the question mentioned above, considering "+str(df_user["user_id"][0])+ "'s hobbies, interests, favorite food and travel plans mentioned above? Do not repeat the cities and countries that "+ str(df_user["user_id"][0])+" is already travelling to. Start your response with Hello "+str(df_user["user_id"][0])
 
-    prompt_text = prompt_initial_text+user_intro_text
+    prompt_text = prompt_initial_text+user_intro_text+user_destinatins
     prompt_text = prompt_text.replace('"', '\\"')
     prompt_format =    """
 
@@ -30,16 +35,16 @@ def createPromt(df_user, df_dest):
     return prompt_template
 
 
-def getUser(userId):
+def getUserPrompt(userId):
     df_user = wr.athena.read_sql_query(sql="SELECT * FROM users_csv where user_id=:userId", 
                                   params={"userId": userId}, database="cruise-hack-01-db-glue")
     activities = df_user["preferred_activities"][0]
     print(activities)
     # Try using params
     df_dest = wr.athena.read_sql_query(
-        sql="SELECT * FROM destination_data_csv where activities_available like '%" + activities  + "%'",
+        sql="SELECT * FROM destination_data_csv d, cruises c where d.port_name = c.port_1 and d.activities_available like '%" + activities  + "%'",
         params={"activities": activities}, database="cruise-hack-01-db-glue")
-    print(df_dest)
+        
     return createPromt(df_user, df_dest)
 
 
@@ -68,7 +73,7 @@ def get_bedrock_chain(user_id):
     )
     claude_llm.model_kwargs = {"temperature": 0.5, "max_tokens_to_sample": 4096}
 
-    prompt_template = getUser(user_id)
+    prompt_template = getUserPrompt(user_id)
 
     pt = PromptTemplate(
         input_variables=["history", "input"], template=prompt_template
@@ -84,7 +89,11 @@ def get_bedrock_chain(user_id):
 
     return conversation
 
-
-print(get_bedrock_chain(1000001))
+def exec_chain(ch, pt):
+    token_ct = ch.llm.get_num_tokens(pt)
+    return ch({"input": pt}), token_ct
+    
+    
+#print(get_bedrock_chain(1000001))
 
 
